7372349ff7b2a852546777b861825f30dd23f36b
fsanchezi <nandosanigle5@hotmail.com>
2015-08-04 12:44:18 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,25 +13,51 @@ exports.load = function(req, res, next, quizId) {
 
 //GET quizes/
 exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
-	}).catch(function(error){next(error)});
+	models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+    }).catch(function(error){next(error)});
 };
 
 //GET quizes/:ID
 exports.show = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: req.quiz});
-	})
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 //GET quizes/:id/answer
-exports.answer = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === req.quiz.respuesta){
-			res.render('quizes/answer', { quiz: quiz, respuesta:'Correcto' });
-		}else{
-			res.render('quizes/answer', { quiz: req.quiz, respuesta:'Incorrecto' });	
-		}		
-	})
+exports.answer = function(req, res) {
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render(
+    'quizes/answer', 
+    { quiz: req.quiz, 
+      respuesta: resultado, 
+      errors: []
+    }
+  );
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // crea objeto quiz 
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+  quiz.validate().then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz.save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }
+    }
+  )
 };
--- models/quiz.js
+++ models/quiz.js
@@ -4,10 +4,16 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define(
     'Quiz',
     { pregunta: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
       },
       respuesta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+      },
+      image: {
         type: DataTypes.STRING
       }
-    });
+    }
+  );
 }
